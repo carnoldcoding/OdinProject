e4db1bfe2d62bb02614bcc85713daa75586a2c84
carnoldcoding <carnoldcoding@gmail.com>
2022-04-17 13:32:27 -0400

    forms

--- Forms/basic_forms.js
+++
@@ -1,7 +0,0 @@
-const username = document.querySelector("#user-input");
-const displayButton = document.querySelector("#display-button");
-const displayOutput = document.querySelector("#display");
-
-displayButton.addEventListener('click', (e) => {
-    displayOutput.innerText = `${username.value}`;
-})
